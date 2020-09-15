8939fa626432e2184a3aefc1536fe186e619eead
William Andersson <54896119+WilliamAndersson0@users.noreply.github.com>
2020-09-15 16:36:53 +0200

    Mapcontroller och ny class Chans

---
+++ src/Chans.java
@@ -0,0 +1,26 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Chans extends Application {
+
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        MapController mapController = new MapController();
+        Parent root = mapController;
+
+        Scene scene = new Scene(root, 1920, 1080);
+
+        stage.setTitle("Chans");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+
+    public static void main(String args[]) {launch(args);
+    }
+}
