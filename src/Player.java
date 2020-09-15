30cf6547aa83050caeab1e896d0521aec3036143
Zneed99 <holmesten99@hotmail.com>
2020-09-15 10:11:57 +0200

    getter and setters for player

--- src/Player.java
+++ src/Player.java
@@ -9,4 +9,28 @@ public class Player {
         this.id = id;
         this.color = color;
     }
+
+    public int getUnits() {
+        return units;
+    }
+
+    public void setUnits(int units) {
+        this.units = units;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
 }
