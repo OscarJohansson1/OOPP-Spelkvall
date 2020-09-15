01db7928ab07abbe71378e62963afce106f1df24
OscarJohansson1 <oscarjohansson999@gmail.com>
2020-09-15 16:17:15 +0200

    Added fxml with map and edited class Space

--- src/Space.java
+++ src/Space.java
@@ -1,2 +1,54 @@
+import javafx.fxml.FXML;
+import javafx.scene.shape.Rectangle;
+
 public class Space {
+
+    private final int id;
+    private Player player;
+    private int units;
+    private final String name;
+    @FXML
+    private final Rectangle cube;
+
+    public Space(int id, Player player, int units, String name, Rectangle cube){
+        this.id = id;
+        this.player = player;
+        this.units = units;
+        this.name = name;
+        this.cube = cube;
+    }
+
+    public void updateSpace(Player player, int units) {
+        setPlayer(player);
+        setUnits(units);
+        // Change color and units
+    }
+
+    public void updateSpace(int units) {
+        updateSpace(this.player, units);
+    }
+
+    private void setPlayer(Player player) {
+        this.player = player;
+    }
+
+    private void setUnits(int n) {
+        units = n;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    public int getUnits() {
+        return units;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
---
+++ test/TestSpace.java
@@ -0,0 +1,15 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import javafx.scene.shape.Rectangle;
+import org.junit.Test;
+
+public class TestSpace {
+
+    @Test
+    public void testUpdateSpace() {
+        //Space space = new Space(1, new Player(100, 1, "Green" ), 10, "Hubben");
+        //space.updateSpace(20);
+
+        //assertEquals(space.getUnits(), 20);
+    }
+}
