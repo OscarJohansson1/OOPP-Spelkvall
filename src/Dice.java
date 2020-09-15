eff9da9f311fad2281a30e423493ce9401de966e
Zneed99 <holmesten99@hotmail.com>
2020-09-15 10:32:26 +0200

    Can roll one dice and choose N dices to roll which gets returned as a result in an ArrayLIst

--- src/Dice.java
+++ src/Dice.java
@@ -1,2 +1,28 @@
+import java.util.ArrayList;
+
 public class Dice {
+
+    private final int NUMBER_RANGE = 6;
+
+
+    //Roll one dice and return a number between 1-6
+    public int rollADice(){
+        int value = (int)(Math.random() * NUMBER_RANGE) + 1;
+        System.out.println(value);
+        return value;
+
+    }
+
+    //Roll N dices and return an ArrayList with the results
+    public ArrayList<Integer> rollNDIces(int n){
+        ArrayList<Integer> results = new ArrayList<>();
+
+        for (int i = 0; i < n; i++){
+            results.add(rollADice());
+        }
+
+        System.out.println(results.toString());
+
+        return results;
+    }
 }
--- src/Risk.java
+++ src/Risk.java
@@ -2,5 +2,11 @@ public class Risk {
 
     public static void main(String args[]) {
 
+        /*
+        //Testing Dice
+        Dice dice = new Dice();
+        dice.rollADice();
+        dice.rollNDIces(20);
+         */
     }
 }
