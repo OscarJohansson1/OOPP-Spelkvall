63cff8b56c626edb762fbfb78bc02d3b77f374bb
Zneed99 <holmesten99@hotmail.com>
2020-09-17 15:28:06 +0200

    Attack is done for the prototype and has been tested.

--- src/Attack.java
+++ src/Attack.java
@@ -3,69 +3,78 @@ import java.util.ArrayList;
 public class Attack {
 
     Dice dice = new Dice();
-    //Space space = new Space();
-    private boolean attack = true;
+    private boolean attack = false;
 
     //This method checks if the attack is possible and declares which space is to be attacked
-    void DeclareAttack(Space mySpace, Space opponentSpace, int myUnits){
+    void DeclareAttack(Space mySpace, Space opponentSpace, int myUnits) {
 
-            if (nextTo(mySpace, opponentSpace) && isAttackPossible(myUnits))
-            {
-                attack = true;
-            }
+        if (nextTo(mySpace, opponentSpace) && isAttackPossible(myUnits)) {
+            attack = true;
+            System.out.println("Valid attack");
+        }
     }
-
 
 
     //This method calculates the attack with the help of dices according to the official rules of Risk.
     //This also declares the winner of the attack
-    void calculateAttack(int myUnits, int opponentUnits){
+    void calculateAttack(int myUnits, int opponentUnits, Space mySpace, Space enemySpace) {
 
-        int myDice = (myUnits -1);
-        int opponentDice = opponentUnits ;
+        int myDice = (myUnits - 1);
+        int opponentDice = opponentUnits;
 
-        if(attack){
+        if (attack) {
 
             ArrayList<Integer> myResults = dice.rollNDIce(myDice);
             ArrayList<Integer> opponentResults = dice.rollNDIce(opponentDice);
 
-            while(myResults.size() > 0 && opponentResults.size() > 0 ){
+            while (myResults.size() > 0 && opponentResults.size() > 0) {
 
-                if(findHighestDie(myResults) > findHighestDie(opponentResults)){
+                if (findHighestDie(myResults) > findHighestDie(opponentResults)) {
+
                     //Kill one unit in opponent space
-                    System.out.println("Die Bitch");
-                }
-                else {
+                    enemySpace.updateSpace(-1);
+                    System.out.println("Defender loses one unit");
+
+                    if (enemySpace.getUnits() < 1){
+                        System.out.println(enemySpace.getPlayer());
+                        enemySpace.updateSpace(mySpace.getPlayer(), mySpace.getUnits() -1);
+                        System.out.println("Units in area are all dead");
+                        System.out.println(mySpace.getPlayer());
+                        System.out.println(enemySpace.getPlayer());
+                    }
+
+                } else {
+
                     //Kill one unit in current player space
-                    System.out.println("Ouch i failed");
+                    mySpace.updateSpace(-1);
+                    System.out.println("Attacker loses one unit");
                 }
             }
         }
     }
 
     //This checks if one space on the board is next to another
-    private boolean nextTo(Space mySpace, Space opponentSpace){
+    private boolean nextTo(Space mySpace, Space opponentSpace) {
 
         return true;
     }
 
-    private boolean isAttackPossible(int myUnits){
+    private boolean isAttackPossible(int myUnits) {
         return myUnits > 1;
     }
 
-     Integer findHighestDie(ArrayList<Integer> rolls){
+    Integer findHighestDie(ArrayList<Integer> rolls) {
 
         int value = 0;
         int index = 0;
 
-        for (int i = 0; i < rolls.size(); i++){
-            if (rolls.get(i) > value){
+
+        for (int i = 0; i < rolls.size(); i++) {
+            if (rolls.get(i) > value) {
                 value = rolls.get(i);
                 index = i;
-
             }
         }
-
         rolls.remove(index);
         //System.out.println("Value:" + value);
         //System.out.println("Index:" + index);
@@ -73,3 +82,6 @@ public class Attack {
         return value;
     }
 }
+
+
+
--- src/Chans.java
+++ src/Chans.java
@@ -2,7 +2,11 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
+
+import java.awt.*;
 
 public class Chans extends Application {
 
@@ -27,9 +31,15 @@ public class Chans extends Application {
     public static void main(String args[]) {
         Attack attack = new Attack();
         Dice dice = new Dice();
+        javafx.scene.shape.Rectangle rect = new Rectangle();
+        //Player player1 = new Player(20, 2, color.BLACK);
+        //Player player2 = new Player(20, 2, color.RED);
+        //Space mySpace = new Space(1, player1, 5, "Hubba", rect);
+        //Space enemySpace = new Space(2, player2, 3,"Basa", rect );
 
         //attack.findHighestDie(dice.rollNDIce(5));
-        attack.calculateAttack(5, 5);
+        //attack.DeclareAttack(mySpace, enemySpace, 3);
+        //attack.calculateAttack(3, 1, mySpace, enemySpace);
 
         launch(args);
 
