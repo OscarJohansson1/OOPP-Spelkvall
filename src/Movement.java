cae5c7171f71bb478b9c9c30e8b10af9d2ff444f
Hedquist <vilhelm.hedquist@hotmail.com>
2020-09-17 15:05:35 +0200

    Made framework for Movement class, needs UI implementation

--- src/Movement.java
+++ src/Movement.java
@@ -1,2 +1,29 @@
 public class Movement {
+
+    private Round currentRound;
+
+    public Movement(Round currentRound) {
+        this.currentRound = currentRound;
+    }
+
+    private boolean validateSpaces(Round currentRound, Space space1, Space space2) {
+        return (currentRound.getCurrentPlayer() == space1.getPlayer()) && (currentRound.getCurrentPlayer() == space2.getPlayer());
+    }
+
+    public void MoveUnits(Space space1, Space space2) {
+        if (validateSpaces(this.currentRound, space1, space2)) {
+            int units1 = space1.getUnits();
+            int units2 = space2.getUnits();
+            int totalUnits = space1.getUnits() + space2.getUnits();
+
+            //TODO insert way for player to determine desired unit spread via the UI
+
+            if (units1 > 0 && units2 > 0 && units1 + units2 == totalUnits) {
+                space1.updateSpace(this.currentRound.getCurrentPlayer(), units1);
+                space2.updateSpace(this.currentRound.getCurrentPlayer(), units2);
+            }
+        } else {
+            System.out.println("Both spaces do not belong to the current player, this message should never be shown.");
+        }
+    }
 }
