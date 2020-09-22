25860ac2984816e8f4c86ea616a1dd76ccf3032e
OscarJohansson1 <oscarjohansson999@gmail.com>
2020-09-22 15:55:23 +0200

    Added test classes and tests for package-private classes in Attack-class

---
+++ test/TestWin.java
@@ -0,0 +1,21 @@
+import org.junit.Test;
+
+public class TestWin {
+
+    @Test
+    public void testDeployment() {
+        //Player p1 = new Player(10, 1, Color.color(20, 20, 20));
+        //Player p2 = new Player(10, 2, Color.color(100, 0, 20));
+
+        //Deployment dp = new Deployment();
+        //dp.startDeployment(p1, 5);
+
+
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testGetBadIndex() {
+        // Test
+    }
+
+}
