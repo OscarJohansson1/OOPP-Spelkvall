547db5b2f5e3af446a4941b20905da3bae9c3e84
OscarJohansson1 <oscarjohansson999@gmail.com>
2020-09-25 10:55:12 +0200

    JavaDoc in ModelDataHandler

--- src/Program/Model/ModelDataHandler.java
+++ src/Program/Model/ModelDataHandler.java
@@ -29,6 +29,12 @@ public class ModelDataHandler {
         });
         round = new Round();
     }
+
+    /**
+     * Get player color based on player id.
+     * @param id id of the player.
+     * @return the color that correspond the player.
+     */
     public Color findPlayerColor(int id)
     {
         return players[id].getColor();
@@ -46,18 +52,32 @@ public class ModelDataHandler {
         board.selectedSpace = null;
         board.selectedSpace2 = null;
     }
+
+    /**
+     * Method that sets selectedSpace/selectedSpace2 to a specific space based on id.
+     * @param id The id of the space to add as a selectedSpace
+     * @return True if a the space was added to a selectedSpace, false if unsuccessful
+     */
     public boolean receiveSelectedSpace(int id)
     {
         if(board.findSpace(id).getPlayer() == currentplayer && board.selectedSpace == null) {
             board.selectedSpace = board.findSpace(id);
             return true;
         }
-        else if((board.selectedSpace !=  null && round.getCurrentPhase().equals(Round.Phase.MOVE) && board.findSpace(id).getPlayer() == currentplayer) || board.selectedSpace != null && board.findSpace(id).getPlayer() != currentplayer && round.getCurrentPhase().equals(Round.Phase.ATTACK)){
+        else if((board.selectedSpace !=  null && round.getCurrentPhase().equals(Round.Phase.MOVE) &&
+                board.findSpace(id).getPlayer() == currentplayer) || board.selectedSpace != null &&
+                board.findSpace(id).getPlayer() != currentplayer && round.getCurrentPhase().equals(Round.Phase.ATTACK)){
             board.selectedSpace2 = board.findSpace(id);
             return true;
         }
         return false;
     }
+
+    /**
+     * Method that changes the currentPlayer to the next player in the player list. If the player is the last player in
+     * the list, the first player in the list is selected as the new currentPlayer.
+     * @param player The currentPlayer.
+     */
     public void nextPlayer(Player player)
     {
         for(int i = 0; i < players.length; i++)
@@ -71,19 +91,38 @@ public class ModelDataHandler {
             }
         }
     }
+
+    /**
+     * Method that returns the id of a player as a String
+     * @return the id of a player as a String.
+     */
     public String getCurrentPlayerName()
     {
         return String.valueOf(currentplayer.getId());
     }
+
+    /**
+     * Method that returns the name of the currentPhase
+     * @return the name of the currentPhase as a String
+     */
     public String getCurrenPhase()
     {
         return round.getCurrentPhase().name();
     }
+
+    /**
+     * Method that change the phase to the next one and reset the selectedSpaces
+     */
     public void nextPhase()
     {
         round.nextPhase();
         resetSelectedSpace();
     }
+
+    /**
+     * Method that checks if the selectedSpaces aren't null and starts the phase if so.
+     * @return if the phase could start successfully.
+     */
     public boolean nextMove()
     {
         if((board.selectedSpace != null && round.getCurrentPhase().equals(Round.Phase.DEPLOY)) || board.selectedSpace2 != null)
@@ -95,10 +134,21 @@ public class ModelDataHandler {
         return false;
     }
 
+    /**
+     * Method that returns the units on a space based on id.
+     * @param id Id of the space that the amount of units should be returned.
+     * @return Number of units on a space, based on id.
+     */
     public int findUnitsOnSpace(int id)
     {
         return board.findSpace(id).getUnits();
     }
+
+    /**
+     * Method that returns the color on a space based on id.
+     * @param id Id of the space that the color should be returned.
+     * @return Color of the space, based on id
+     */
     public Color getColorOnSpace(int id)
     {
         return board.findSpace(id).getPlayer().getColor();
