90c0620839221bfd9a4466afbb10d203a415aa36
OscarJohansson1 <oscarjohansson999@gmail.com>
2020-09-25 11:39:28 +0200

    JavaDoc in View

--- src/Program/View/View.java
+++ src/Program/View/View.java
@@ -19,15 +19,33 @@ public class View extends AnchorPane {
 
     public void setUpMap(){
     }
+
+    /**
+     * Method that creates a list with the texts on spaces.
+     * @param mapController The mapController used in the project.
+     */
     public void setUpStart(MapController mapController){
 
         texts = new Text[] { null,mapController.text1,mapController.text2,mapController.text3,mapController.text4};
     }
+
+    /**
+     * Method that updates the player and phase texts.
+     * @param playername The name of the currentPlayer.
+     * @param currentPhase The current phase.
+     * @param mapController The mapController used in the project.
+     */
     public void updatePhasePlayerText(String playername, String currentPhase, MapController mapController)
     {
         mapController.playerText.setText("Currentplayer: " + playername + "");
         mapController.phaseText.setText(currentPhase);
     }
+
+    /**
+     * Method that update the amount of units-text based on id of space.
+     * @param id Id of space that the amount of units should be updated on.
+     * @param units The new amount of units.
+     */
     public void updateTextUnits(int id, int units)
     {
         for(int i = 0; i < texts.length; i++)
@@ -40,6 +58,13 @@ public class View extends AnchorPane {
             }
         }
     }
+
+    /**
+     * Method that sets the color an the spaces.
+     * @param cube The cube representing a space.
+     * @param color The color of the space.
+     * @param mapController The mapController used in the project.
+     */
     public void setColor(Rectangle cube, Color color, MapController mapController)
     {
         if(cube == mapController.cube1)
@@ -56,6 +81,12 @@ public class View extends AnchorPane {
             mapController.cube4.setFill(color);
         }
     }
+
+    /**
+     * Method that resets the colors to a standard colors.
+     * @param colors An array with colors.
+     * @param mapController The mapController used in the project.
+     */
     public void resetColor(Color[] colors, MapController mapController)
     {
         mapController.cube1.setFill(colors[1]);
@@ -63,6 +94,12 @@ public class View extends AnchorPane {
         mapController.cube3.setFill(colors[3]);
         mapController.cube4.setFill(colors[4]);
     }
+
+    /**
+     * Method that makes the correct phase-pane visible.
+     * @param string The name of the phase that should be displayed.
+     * @param mapController The mapController used in the project.
+     */
     public void updatePhase(String string, MapController mapController)
     {
         switch (string)
