7d57efe9c364be9510ff68edc204b97b5e3d5b1c
EpicMunz <mans.josefsson99@gmail.com>
2020-10-01 12:32:36 +0200

    Skapat endMenu + pauseMenu

--- src/Program/Controller/pauseController.java
+++ src/Program/Controller/pauseController.java
@@ -6,16 +6,38 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
-
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 public class pauseController extends AnchorPane {
 
     @FXML
-    private Button pauseButton;
+    private HBox pauseHBox;
+
+    @FXML
+    private ImageView chanceImageView;
+
+    @FXML
+    private Pane buttonPane;
+
+    @FXML
+    private Button returnButton;
+
+    @FXML
+    private Button newGameButton;
+
+    @FXML
+    private Button endGameButton;
+
+    @FXML
+    private Button quitGameButton;
 
     MapController mapController = new MapController();
     Parent root = mapController;
@@ -37,20 +59,51 @@ public class pauseController extends AnchorPane {
 
     }
 
-    private void initialize() {
-        pauseButton.setOnMouseClicked(new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent mouseEvent) {
+    public void initialize() {
+        //TODO: What now? Pause ska finnas från start av programmet och ska sedan finnas kvar i ett lager bakom kartan.
 
-                Scene scene = new Scene(root, 800, 600);
-
-                stage.setTitle("Pause");
-                stage.setScene(scene);
-                stage.show();
-
-            }
-        });
     }
+
+    public void returnButtonPressed(){
+
+        /*
+        //Uppdatera kartan
+        updateMap();
+        //Lägg kartan längst fram
+        map.toFront();
+        //Visa interfacen igen
+        interfacePane.setVisible(true);
+         */
+    }
+
+    public void newGameButtonPressed(){
+
+        /*
+        //Avsluta omgången
+        game.end();
+        //Gå till setUpGame.fxml
+        ... hur man nu skriver det
+         */
+    }
+
+    public void endGameButtonPressed(){
+
+         /*
+        //Avsluta omgången
+        game.end();
+        //Gå till endMenu.fxml
+        ... hur man nu skriver det
+         */
+    }
+
+    public void quitGameButtonPressed(){
+
+        /*
+        //Gå till endMenu.fxml
+        ... hur man nu skriver det
+         */
+    }
+
 
 
 }
