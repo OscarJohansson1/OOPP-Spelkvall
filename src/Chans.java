2c1efdf558a6559ce608f9c1a51742fa03df1b29
Zneed99 <holmesten99@hotmail.com>
2020-10-02 16:05:18 +0200

    Cleaned up code in Chans

--- src/Chans.java
+++ src/Chans.java
@@ -5,12 +5,8 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Chans extends Application {
-
-
-
     @Override
     public void start(Stage stage) throws Exception {
-
         StartController startController = new StartController(stage);
         Parent start = startController;
 
@@ -21,21 +17,7 @@ public class Chans extends Application {
         stage.show();
     }
 
-
     public static void main(String args[]) {
-
-        //Program.View.View.Model.Player player1 = new Program.View.View.Model.Player(20, 2, color.BLACK);
-        //Program.View.View.Model.Player player2 = new Program.View.View.Model.Player(20, 2, color.RED);
-        //Program.View.View.Model.Space mySpace = new Program.View.View.Model.Space(1, player1, 5, "Hubba", rect);
-        //Program.View.View.Model.Space enemySpace = new Program.View.View.Model.Space(2, player2, 3,"Basa", rect );
-
-        //attack.findHighestDie(dice.rollNDIce(5));
-        //attack.DeclareAttack(mySpace, enemySpace, 3);
-        //attack.calculateAttack(3, 1, mySpace, enemySpace);
-
         launch(args);
-
-
-
     }
 }
