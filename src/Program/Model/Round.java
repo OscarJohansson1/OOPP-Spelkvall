2527bb72fcad227198a74b7eda97e0538fc12952
Zneed99 <holmesten99@hotmail.com>
2020-10-02 09:50:00 +0200

    Commented Player
    Commented Round

--- src/Program/Model/Round.java
+++ src/Program/Model/Round.java
@@ -2,10 +2,19 @@ package Program.Model;
 
 import javafx.scene.paint.Color;
 
+/**
+ * This class is controlling the different stages in one round. The method StartPhase() is always called
+ * when the phases Deploy, Attack or Move is called in ModelDataHandler
+ */
+
 class Round {
 
     private Phase currentPhase = Phase.DEPLOY;
 
+    /**
+     * This enum is used to check what state the game is in to be able to create an order
+     * for the different stages in one round
+     */
     public enum Phase {
 
         DEPLOY, ATTACK, MOVE, END;
