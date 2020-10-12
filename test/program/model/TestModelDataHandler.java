a55c356053ef281a1022060a8c57a010de470d80
OscarJohansson1 <oscarjohansson999@gmail.com>
2020-10-12 10:16:11 +0200

    Added test to modelDataHandler

--- test/program/model/TestModelDataHandler.java
+++ test/program/model/TestModelDataHandler.java
@@ -9,6 +9,29 @@ public class TestModelDataHandler {
 
     @Test
     public void testNextPhase() {
-        assertEquals(1, 1);
+        ModelDataHandler mdh = ModelDataHandler.getModelDataHandler();
+        int n = 27;
+        int player1 = 0;
+        int player2 = 0;
+        int player3 = 0;
+        JFXPanel jfxPanel = new JFXPanel();
+
+        mdh.initialize(n, new ArrayList<>(Arrays.asList(Color.rgb(1,0,0),
+                Color.rgb(0,1,0), Color.rgb(0,0,1))), new ArrayList<>(Arrays.asList(
+                "it_logo", "a_logo", "d_logo"
+        )));
+
+        for(int i = 0; i < n; i++){
+            if(Color.rgb(1, 0, 0).equals(mdh.getColorOnSpace(i))){
+                player1++;
+            } else if(Color.rgb(0, 1, 0).equals(mdh.getColorOnSpace(i))){
+                player2++;
+            } else {
+                player3++;
+            }
+        }
+        assertTrue((Math.abs(player1 - player2) <= 1) && (Math.abs(player2 - player3) <= 1) && (Math.abs(player1 - player3) <= 1),
+                "Players have: " + player1 + ", " + player2 + ", " + player3 + " spaces respective");
     }
+
 }
