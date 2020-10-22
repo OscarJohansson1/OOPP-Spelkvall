e3bed565525c71368c96f08434dbed3040b8a656
Hedquist <vilhelm.hedquist@hotmail.com>
2020-10-22 15:14:39 +0200

    Updated TestAttackPhase to test current methods

--- src/test/java/program/model/TestAttackPhase.java
+++ src/test/java/program/model/TestAttackPhase.java
@@ -12,6 +12,8 @@ import java.util.List;
 
 public class TestAttackPhase {
 
+    AttackPhase attackPhase = new AttackPhase();
+
     private Player player1;
     private Player player2;
 
@@ -20,7 +22,6 @@ public class TestAttackPhase {
 
     @Before
     public void before(){
-
         player1 = new Player(10,1, "#123123", "hej", "hej");
         player2 = new Player(10,2, "#123123", "hej", "hej");
 
@@ -29,39 +30,7 @@ public class TestAttackPhase {
     }
 
     @Test
-    public void testAmountOfDice(){
-        //List<Integer> dice = Attack.calculateAttack(space1, space2);
-        //assertEquals(5, dice.size(), "5 dice should've been used, but only " + dice.size() + " was");
-    }
-
-    @Test
-    public void testAmountOfDiceWhenFewUnits(){
-        space1.updateSpace(3);
-        space1.updateSpace(1);
-        //List<Integer> dice = Attack.calculateAttack(space1, space2);
-        //assertEquals(3, dice.size(), "3 dice should've been used, but only " + dice.size() + " was");
-    }
-
-    @Test
-    public void testCalculateAttackUnitsLeft(){
-        //Attack.calculateAttack(space1, space2);
-        if (space1.getPlayer() == space2.getPlayer()){
-            assertTrue(space1.getUnits() < 5, "Successful attack, but 5 or more units left on space1");
-        } else {
-            assertTrue(space1.getUnits() > 0, "Less than 1 unit left after unsuccessful attack");
-        }
-    }
-
-    @Test
-    public void testFindHighestDieRightReturnValue(){
-        //int highest = Attack.findHighestDie(MockDice.rollNDIce(5, 6));
-        //assertEquals(6, highest, "Wrong value returned");
-    }
-
-    @Test
-    public void testFindHighestDieListLength(){
-        ArrayList<Integer> list = MockDice.rollNDIce(5, 6);
-        //Attack.findHighestDie(list);
-        assertEquals(4, list.size(), "List doesn't have the correct length after findHighestDie call");
+    public void testStartPhase(){
+        assertTrue(attackPhase.startPhase(space1, space2, player1, 0), "Space is null, or attack not possible");
     }
 }
