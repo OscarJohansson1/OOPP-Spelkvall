8cd7aa1037545de2fea5cc15cf264d902d85d5ed
William <williama.andersson@gmail.com>
2020-10-23 15:36:17 +0200

    chans eventhandler -> lambda

--- src/main/java/program/Chans.java
+++ src/main/java/program/Chans.java
@@ -2,11 +2,9 @@ package program;
 
 import javafx.application.Application;
 import javafx.application.Platform;
-import javafx.event.EventHandler;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import javafx.stage.WindowEvent;
 import program.controller.StartController;
 
 import java.awt.*;
@@ -24,24 +22,19 @@ public class Chans extends Application {
         Scene menu = new Scene(start, screenSize.width, screenSize.height);
 
 
-        stage.setOnCloseRequest(new EventHandler<WindowEvent>() {
-            @Override
-            public void handle(WindowEvent windowEvent) {
-                Platform.exit();
-                System.exit(0);
-            }
+        stage.setOnCloseRequest(windowEvent -> {
+            Platform.exit();
+            System.exit(0);
         });
         stage.setFullScreen(true);
 
         stage.setTitle("Menu");
         stage.setScene(menu);
         stage.show();
-        stage.setOnCloseRequest(new EventHandler<WindowEvent>() {
-            public void handle(WindowEvent we) {
-                System.out.println("Stage is closing");
-                Platform.exit();
-                System.exit(0);
-            }
+        stage.setOnCloseRequest(we -> {
+            System.out.println("Stage is closing");
+            Platform.exit();
+            System.exit(0);
         });
 
     }
